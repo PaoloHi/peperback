75727dd36877f23a4bf356c8c2db308a9c606a4a
Paolo Fernando <frpaolofernando@gmail.com>
2023-12-18 09:34:33 -0600

    vistas

---
+++ Views/Shared/_modalAprobarRechazar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace adminRummet.Views.Shared
+{
+    public class _modalAprobarRechazarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
